fdef1c35c4f5def6aed97225c66aad09edb1a379
UsmanAli3578 <usmanali.3578@gmail.com>
2026-08-16 19:25:22 +0500

    Debug Railway database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
 
 const pool = new Pool({
 	user: process.env.DB_USER,
