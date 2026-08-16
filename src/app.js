754ab0c68cfd20944447bf21c484401b16aae626
UsmanAli3578 <usmanali.3578@gmail.com>
2026-08-16 18:36:43 +0500

    Add product update and delete features

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ const cors = require('cors');
 const app = express();
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		credentials: true,
+	}),
+);
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -34,4 +34,78 @@ async function getProducts(req, res) {
 	});
 }
 
-module.exports = { createProduct, getProducts };
+async function getProductsBySellerId(req, res) {
+	const seller_id = req.user.id;
+	const products = await productModel.getProductsBySellerId(seller_id);
+
+	return res.status(200).json({
+		products,
+	});
+}
+
+async function editProduct(req, res) {
+	const product_id = req.params.id;
+	const seller_id = req.user.id;
+
+	const { name, description, price } = req.body;
+	let image = null;
+
+	if (req.file) {
+		const uploadedImage = await imagekit.files.upload({
+			file: req.file.buffer.toString('base64'),
+			fileName: req.file.originalname,
+			folder: '/products',
+		});
+
+		image = uploadedImage.url;
+	}
+
+	const updatedProduct = await productModel.EditProduct(
+		product_id,
+		seller_id,
+		name,
+		description,
+		price,
+		image,
+	);
+
+	if (!updatedProduct) {
+		return res.status(404).json({
+			message: 'Product not found or you are not the owner',
+		});
+	}
+
+	return res.status(200).json({
+		message: 'Product updated successfully',
+		product: updatedProduct,
+	});
+}
+
+async function deleteProduct(req, res) {
+	const product_id = req.params.id;
+	const seller_id = req.user.id;
+
+	const deletedProduct = await productModel.DeleteProduct(
+		product_id,
+		seller_id,
+	);
+
+	if (!deletedProduct) {
+		return res.status(404).json({
+			message: 'Product not found or you are not the owner',
+		});
+	}
+
+	return res.status(200).json({
+		message: 'Product deleted successfully',
+		product: deletedProduct,
+	});
+}
+
+module.exports = {
+	createProduct,
+	getProducts,
+	getProductsBySellerId,
+	editProduct,
+	deleteProduct,
+};
