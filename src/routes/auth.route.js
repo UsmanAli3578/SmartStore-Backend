c575ea6f396e448ec6d444899a9e904d619ab86b
UsmanAli3578 <usmanali.3578@gmail.com>
2026-08-20 14:32:52 +0500

    Add logout route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/register', authController.userRegister);
 router.post('/login', authController.userLogin);
 router.get('/me', authMiddleware, authController.getCurrentUser);
+router.post('/logout', authController.userLogout);
 
 module.exports = router;
